26b1c4799f8ea2954c138b994af41dfe1520d8e7
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-04 20:42:45 +0530

    feat:added number_python script.

---
+++ 01_Basics/numbers_python.py
@@ -0,0 +1,12 @@
+x = 2
+y = 5
+z = 7
+b = 2.03
+print(x+z)
+# Always use parenthesis for any mathematical operation.
+a = (x+y)*z
+print(a)
+ # print(b+x) # this will perform the operation but you should not use this as these two are two different data types.
+# Instead use same data types for addition operation.
+b = int(b)
+print(b+x)
