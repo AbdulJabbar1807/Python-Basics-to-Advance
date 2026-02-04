09f77c4807d60904d600c14dfe18ae8835d55254
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-05 05:22:26 +0530

    docs:minor update in comment.

--- 01_Basics/numbers_python.py
+++ 01_Basics/numbers_python.py
@@ -6,7 +6,7 @@ print(x+z)
 # Always use parenthesis for any mathematical operation.
 a = (x+y)*z
 print(a)
- # print(b+x) # this will perform the operation but you should not use this as these two are two different data types.
+ # print(b+x) # this will perform the operation but its better not to use this as these two are two different data types.
 # Instead use same data types for addition operation.
 b = int(b)
 print(b+x)
