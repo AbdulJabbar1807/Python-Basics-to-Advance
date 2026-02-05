0ebc161cfe0db6d6f8f51b20c3426ee1ac668f86
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-05 08:48:33 +0530

    feat:added sets_python file.

---
+++ 02_Numbers/sets_python.py
@@ -0,0 +1,9 @@
+set_one = {3,7,9,11}
+set_two = set_one | {5,6}
+print(set_two)
+set_three = set_one & {3,7}
+print(set_three)
+set_four = set_two & set_three
+print(set_four)
+set_five = set_one & {1,2}
+print(set_five)
