cd01f3d7b80125f5093342c6df0cd8799553eeae
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-06 11:16:05 +0530

    feat:list in python.

---
+++ 04_List/list_python.py
@@ -0,0 +1,27 @@
+coffee_type = ['Arabica','Robusta','Liberica']
+print(coffee_type)
+print(coffee_type[0])
+print(coffee_type[0:2])
+
+coffee_type[2] = 'Excelsa'
+print(coffee_type)
+
+print(coffee_type[1:2])# will provide string at index 1 in list.
+coffee_type[1:2] = 'Liberica'# will place Liberica at index 1 in list but with each character separeted by ", ".
+print(coffee_type)
+coffee_type[0:1] = 'Arabica'
+print(coffee_type)
+
+coffee_type[0:2] = ['Arabic','Liberica'] # both values will be treated as an array elements
+print(coffee_type)
+
+coffee_type = ['Arabica','Robusta','Liberica']
+print(coffee_type)
+
+print(coffee_type[1:1])
+coffee_type[1:1] = ['beans','beans','beans']
+print(coffee_type)
+print(coffee_type[1:2])
+print(coffee_type[2])
+coffee_type[1:4] = [] # deletion operation
+print(coffee_type)
