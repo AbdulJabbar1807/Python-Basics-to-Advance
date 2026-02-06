aaa3a33bc19674722e5e5211356931e40b752f83
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-06 11:35:40 +0530

    style:identation corrected.

--- 04_List/list_python.py
+++ 04_List/list_python.py
@@ -40,9 +40,12 @@ coffee_type.append("Excelsa")
 print(coffee_type)
 if "Excelsa" in coffee_type:
     print("Yes,Excelsa is there.")
+
 coffee_type.pop()
 print(coffee_type)
+
 coffee_type.remove("Robusta")
 print(coffee_type)
+
 coffee_type.insert(1,"Robusta")
 print(coffee_type)
