0982dfca2812d84b69b469e768862817f6c534d1
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-06 11:34:26 +0530

    feat:list methods.

--- 04_List/list_python.py
+++ 04_List/list_python.py
@@ -31,4 +31,18 @@ coffee_type = ['Arabica','Robusta','Liberica']
 for coffee in coffee_type:
     print(coffee)
 for coffee in coffee_type:
-    print(coffee,end=",")
+    print(coffee,end=" ")
+print("")
+    
+# List methods in python.
+coffee_type = ['Arabica','Robusta','Liberica']
+coffee_type.append("Excelsa")
+print(coffee_type)
+if "Excelsa" in coffee_type:
+    print("Yes,Excelsa is there.")
+coffee_type.pop()
+print(coffee_type)
+coffee_type.remove("Robusta")
+print(coffee_type)
+coffee_type.insert(1,"Robusta")
+print(coffee_type)
