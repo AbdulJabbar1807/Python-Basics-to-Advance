e6f228c165e6b5a8fa4f83917eacd8f54834ceb8
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-06 09:52:53 +0530

    feat:string formatting.

--- 03_Strings/strings_python.py
+++ 03_Strings/strings_python.py
@@ -27,6 +27,9 @@ print(name.upper())
 print(name.capitalize())
 print(name.casefold())
 print(name.find("Jabbar"))
+print(len(name))
+for character in name:
+    print(character)
 
 print(branch.strip())
 print(branch.split())
@@ -35,12 +38,23 @@ print(coffee.split())
 print(coffee.split(" , "))
 print(coffee.replace("Americano","Cappuccino")) 
 
+print("Abdul" in name)
+
 # Formatting in string
 coffee = "Black coffee"
 order = "I oredered one cup of {}."
 print(order.format(coffee))
 
+print("Abdul Jabbar \"I am an AIML engineer.\"")
+
+window_path_01 = "C:\\Users\\Abdul Jabbar\\OneDrive\\Desktop"
+print(window_path_01)
+# ---- OR ----
+window_path_02 = r"C:\Users\Abdul Jabbar\OneDrive\Desktop"
+print(window_path_02)
+
 # list to string
 coffee_vareity = ['Blackcoffee','cappuccino','Coldcoffee']
 print(coffee_vareity)
 print(" , ".join(coffee_vareity))
+
