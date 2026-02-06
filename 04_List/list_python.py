1cd3df570eef0e956ecfe5c4f3edc82c5aa82e42
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-06 12:03:08 +0530

    fix:minor fixes.

--- 04_List/list_python.py
+++ 04_List/list_python.py
@@ -57,8 +57,3 @@ print(coffee_type)
 coffee_type_copy.append("Excelsa")
 print(coffee_type_copy)
 
-# list comrehension in python
-square_num = [x**2 for x in range(10)]
-print(square_num)
-cube_num = [y**3 for y in range(10)]
-print(cube_num)
