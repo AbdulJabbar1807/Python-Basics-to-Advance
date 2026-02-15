4615dff3625fd71bc266a1a6ce9d8be672c46b12
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-15 15:18:54 +0530

    fix:minor fixes.

--- 04_List/list_comprehension.py
+++ 04_List/list_comprehension.py
@@ -3,3 +3,11 @@ square_num = [x**2 for x in range(10)]
 print(square_num)
 cube_num = [y**3 for y in range(10)]
 print(cube_num)
+
+raw_data = "  abdul ,  SAliM,   jOhn  ,  mArYam  "
+filtered_data = [filtered.strip().capitalize() for filtered in raw_data.split(",")]
+print(filtered_data)
+
+fruits = "apple#banana#cherry#orange"
+filtered = [fruit.strip().upper() for fruit in fruits.split("#")]
+print(filtered)
