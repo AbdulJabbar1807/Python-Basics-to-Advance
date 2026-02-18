6951ce8654da20cb9554b27e54e6387670b1150f
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-18 10:58:40 +0530

    feat:dicts in python.

---
+++ 05_Dictionary/dicts_python.py
@@ -0,0 +1,11 @@
+car_brand = {"land cruiser":"defender","Audi":"Q7","Mercedes":"Benz","Tata":"Harrier"}
+print(car_brand)
+
+player_info = {"India":{"Virat":"Kohli","Rohit":"Sharma"},"Australia":{"Steve":"Smith","Glenn":"Maxwell"},
+                "New Zealand":{"Kane":"Williamson","Tom":"Lathem"}}
+print(player_info)
+print(player_info["India"])
+print(player_info["India"]["Virat"])
+
+squarred_num = {x:x**2 for x in range(1,6)}
+print(squarred_num)
