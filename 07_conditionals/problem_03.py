bcff725342fa32fdb7446f7817a9f1b85069dfd7
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-22 17:42:15 +0530

    feat:problem 03.

---
+++ 07_conditionals/problem_03.py
@@ -0,0 +1,15 @@
+Marks = int(input("Enter your grade : "))
+
+if Marks >= 90:
+    print("Your grade is 'A'.")
+elif Marks >= 80:
+    print("Your grade is 'B'.")
+elif Marks >= 70:
+    print("Your grade is 'C'.")
+elif Marks >= 60:
+    print("Your grade is 'D'.")
+else:
+    print("You are failed.")
+
+    
+    
