e238b523e66a2ed7ad7f3259898a6a971d9e451b
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-24 14:38:13 +0530

    feat:problem 04

---
+++ 07_conditionals/problem_04.py
@@ -0,0 +1,21 @@
+Fruit = input("Enter your fruit name : ")
+Fruit_color = input("Enter your fruit color : ")
+
+if Fruit.capitalize() == "Banana":
+    if Fruit_color.capitalize() == "Yellow":
+        print("Ripe")
+    elif Fruit_color.capitalize() == "Green":
+        print("Unripe")
+    elif Fruit_color.capitalize() == "Brown":
+        print("Overripe")
+        
+if Fruit.capitalize() == "Apple":
+    if Fruit_color.capitalize() == "Yellow":
+        print("Ripe")
+    elif Fruit_color.capitalize() == "Green":
+        print("Unripe")
+    elif Fruit_color.capitalize() == "Brown":
+        print("Overripe")
+        
+if Fruit != "Banana" or Fruit != "Apple":
+    print("Sorry,we dont have any information for that.")
