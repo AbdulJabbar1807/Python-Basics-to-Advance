031054410a02f65ee1b0257dc690e3109addb88f
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-03-11 17:50:27 +0530

    feat:problem 06.

--- 08_loops/problem_05.py
+++ 08_loops/problem_05.py
@@ -2,5 +2,7 @@
 char = input("Enter the character : ")
 
 for ch in char :
+    print(ch)
     if char.count(ch) == 1:
         print(f"Non repeated char is : {ch}")
+        break
---
+++ 08_loops/problem_06.py
@@ -0,0 +1,9 @@
+# Factorial calculator
+number = int(input("Enter the number : "))
+factorial = 1
+
+while number > 0:
+    factorial = number * factorial
+    number = number - 1
+
+print("Factorial of the given number is : ",factorial)
